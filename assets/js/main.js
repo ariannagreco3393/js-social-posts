eaa5f400b9292e1f0adb8c9679fe9207b4d23d79
ariannagreco3393 <a.greco3393@gmail.com>
2022-04-12 16:08:28 +0200

    add milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,24 +28,74 @@ const posts = [
     {
         id: 2,
         nome: 'Sofia Perlari',
-        foto: 'https://i.picsum.photos/id/354/300/300.jpg?hmac=nrFV2skCGXr9LuJ58pBJDNjJWUYdjKYSWyVR99laXNA',
+        foto: 'https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ',
         data: '3/9/2021',
         testo: 'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        img: 'https://i.picsum.photos/id/354/300/300.jpg?hmac=nrFV2skCGXr9LuJ58pBJDNjJWUYdjKYSWyVR99laXNA',
+        img: 'https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68',
         likes: 120
     },
 
     {
         id: 3,
         nome: 'Arianna Greco',
-        foto: 'https://i.picsum.photos/id/354/300/300.jpg?hmac=nrFV2skCGXr9LuJ58pBJDNjJWUYdjKYSWyVR99laXNA',
+        foto: 'https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE',
         data: '12/4/2021',
         testo: 'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        likes: 120
-    }
+        img: ' ',
+        likes: 12
+    },
+
 ]
 
 /* 
 Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 */
+
+//seleziono elemento della DOM che contiene i post
+
+const postsElement = document.querySelector('.posts')
+console.log(postsElement);
+
+//uso foreach per il ciclo
+
+const post = posts.forEach((element)=>{
+    //console.log(element.id, element.nome, element.foto);
+
+   /*  const id = element.id
+    const nome = element.nome
+    const foto = element.foto
+    const data = element.data
+    const testo = element.testo
+    const img = element.img
+    const likes = element.likes */
+    
+    //console.log(id, nome, foto, data, testo, img, likes);
+    const postMarkup = `
+    <div class="post">
+                    <div class="heading">
+                        <div class="heading_foto">
+                            <img class="foto_user"
+                                src="${element.foto}"
+                                alt="">
+                        </div>
+                        <div class="heading_text">
+                            <div class="nome">${element.nome}</div>
+                            <div class="data">${element.data}</div>
+                        </div>
+                    </div>
+                    <p class="post_text">${element.testo}.</p>
+                    <div class="img_post">
+                        <img src="${element.img}"
+                            alt="">
+                    </div>
+                    <div class="post_footer">
+                        <button class="btn"><i class="fa-solid fa-thumbs-up"></i>Mi Piace</button>
+                        <p class="likes_number">Piace a ${element.likes} persone</p>
+                    </div>
+                </div>
+    `
+
+    postsElement.insertAdjacentHTML('beforeend', postMarkup)
+    
+})
